dcc8f6c9213b8a814c6ff0fda790f1a2587bc495
Thiago Mota <euthiagojose2004@gmail.com>
2024-11-10 15:36:32 -0300

    firt commit

---
+++ src/main/java/br/com/SistemaControleInsumos/SistemaControleInsumosApplication.java
@@ -0,0 +1,13 @@
+package br.com.SistemaControleInsumos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaControleInsumosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaControleInsumosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/SistemaControleInsumos/SistemaControleInsumosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.SistemaControleInsumos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaControleInsumosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
