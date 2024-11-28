1587c19da6a04002739200ecd0e9c10e8033a03e
juniorferreira23 <junior.ferreira@outlook.com>
2024-11-28 20:12:41 -0300

    FEAT: add teste supplierService  and logs userService

--- src/main/java/br/com/SistemaControleInsumos/SistemaControleInsumosApplication.java
+++ src/main/java/br/com/SistemaControleInsumos/SistemaControleInsumosApplication.java
@@ -1,13 +1,18 @@
 package br.com.SistemaControleInsumos;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SistemaControleInsumosApplication {
 
+	private static  Logger log = LoggerFactory.getLogger(SistemaControleInsumosApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaControleInsumosApplication.class, args);
+		log.info("Aplicação iniciada com sucesso!");
 	}
 
 }
--- src/main/java/br/com/SistemaControleInsumos/services/UserService.java
+++ src/main/java/br/com/SistemaControleInsumos/services/UserService.java
@@ -6,6 +6,8 @@ import br.com.SistemaControleInsumos.entities.User;
 import br.com.SistemaControleInsumos.enuns.UserRole;
 import br.com.SistemaControleInsumos.repositories.UserRepository;
 import br.com.SistemaControleInsumos.dtos.user.RequestUserDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +20,7 @@ import java.util.*;
 @Service
 public class UserService {
 
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
     @Autowired
     UserRepository userRepository;
 
@@ -25,13 +28,16 @@ public class UserService {
     private BCryptPasswordEncoder passwordEncoder;
 
     public ResponseUserDto createUser(RequestUserDto userDto) {
+        log.info("Iniciando criação de usuário. Email: {}", userDto.email());
 
         Optional<User> existingUser = userRepository.findByEmail(userDto.email());
         if (existingUser.isPresent()) {
+            log.warn("Usuário com o email {} já existe!", userDto.email());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User with this email already exists");
         }
 
         if (!userDto.password().equals(userDto.confirmPassword())) {
+            log.warn("As senhas não coincidem para o usuário com email: {}", userDto.email());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The passwords is not equals.");
         }
 
@@ -55,11 +61,13 @@ public class UserService {
                 userSaved.getRole()
         );
 
+        log.info("Usuário criado com sucesso! ID: {}, Email: {}", userSaved.getId(), userSaved.getEmail());
         return response;
 
     }
 
     public List<ResponseUserDto> findAll() {
+        log.info("Iniciando busca por todos os usuários");
         try {
             List<User> users = this.userRepository.findAll();
 
@@ -75,6 +83,7 @@ public class UserService {
                 );
                 responseDtos.add(responseUserDto);
             }
+            log.info("Lista de usuários resgatada com sucesso!");
             return responseDtos;
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
@@ -100,7 +109,7 @@ public class UserService {
                     user.getRole()
             );
 
-
+            log.info("Usuário encontrado com sucesso!");
             return response;
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
@@ -110,18 +119,22 @@ public class UserService {
     }
 
     public ResponseUserDto update(UUID id, UpdateUserDto updateUserDto) {
+        log.info("Iniciando atualização do usuário");
 
         Optional<User> optionalUser = this.userRepository.findById(id);
         if (optionalUser.isEmpty()) {
+            log.warn("Usuário não encontrado");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User not found!");
         }
         User userToUpdate = optionalUser.get();
 
         if (updateUserDto.password() != null && updateUserDto.confirmPassword() != null) {
             if (!passwordEncoder.matches(updateUserDto.oldPassword(), userToUpdate.getPassword())) {
+                log.warn("Senha atual incorreta");
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Incorrect old password.");
             }
             if (!updateUserDto.password().equals(updateUserDto.confirmPassword())) {
+                log.warn("Senhas e confirmação imcompativeis");
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Passwords do not match.");
             }
             userToUpdate.setPassword(passwordEncoder.encode(updateUserDto.password()));
@@ -139,6 +152,7 @@ public class UserService {
 
         User userUpdated = this.userRepository.save(userToUpdate);
 
+        log.info("Usuário atualizado com sucesso!");
         return new ResponseUserDto(
                 userUpdated.getId(),
                 userUpdated.getCreateAt(),
@@ -151,12 +165,16 @@ public class UserService {
 
 
     public Boolean delete(UUID id) {
+        log.info("Iniciando exlusão do usuário");
+
         try {
             Optional<User> optionalUser = this.userRepository.findById(id);
             if (optionalUser.isEmpty()) {
+                log.warn("Usuário não encontrado");
                 throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
             }
             this.userRepository.delete(optionalUser.get());
+            log.info("Usuário deletado com sucesso!");
             return true;
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
@@ -165,6 +183,7 @@ public class UserService {
     }
 
     public List<ResponseUserDto> dateFilter(Timestamp initialDate, Timestamp finishDate) {
+        log.info("Iniciando busca usuario por filtro de data");
         try {
             List<User> users = this.userRepository.findByCreateAtBetween(
                     initialDate,
@@ -183,6 +202,8 @@ public class UserService {
                 );
                 responseDtos.add(responseUserDto);
             }
+
+            log.info("Uuários encontrado por data com sucesso!");
             return responseDtos;
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
